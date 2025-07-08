86b53350d53541c7c8e56129eddfff63015173d7
Sinavet <zerefdparhnjl@gmail.com>
2025-07-08 21:03:29 +0500

    Update Recon2.py

--- Recon2.py
+++ Recon2.py
@@ -37,7 +37,7 @@ def get_gdrive_service():
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
+            creds = flow.run_console()
         with open('token.pickle', 'wb') as token:
             pickle.dump(creds, token)
     return build('drive', 'v3', credentials=creds)
